058baab71125bf703faf31fa2a3ee6cc309a9bfa
anastasia <anastasia.gribovskaia@gmail.com>
2023-01-07 02:58:21 +0300

    feat: displaying items in the shopping cart

--- src/ts/cart.ts
+++ src/ts/cart.ts
@@ -1,5 +1,5 @@
 import { ICartList, ICartGood } from './interfaces';
-import { goodsList } from './goods-list';
+import { goodsList2 } from './goods-list2';
 
 let cartActive: boolean;
 
@@ -11,10 +11,12 @@ function cartOpen () {
     const cartImg = document.querySelector('.basket') as HTMLDivElement;
     const cartPopUp = document.querySelector('.cart') as HTMLDivElement;
     const main = document.querySelector('.main') as HTMLElement;
+    const goodsDetails = document.querySelector('.goods__details') as HTMLElement;
 
     cartImg.addEventListener('click', () => {
         cartPopUp.classList.add('cart_active');
         main.classList.add('main_hidden');
+        goodsDetails.style.display = 'none';
     })
 }
 
@@ -23,10 +25,12 @@ function cartClose () {
     const cartPopUp = document.querySelector('.cart') as HTMLDivElement;
     const logo = document.querySelector('.logo') as HTMLDivElement;
     const main = document.querySelector('.main') as HTMLElement;
+    const goodsDetails = document.querySelector('.goods__details') as HTMLElement;
 
     logo.addEventListener('click', () => {
         cartPopUp.classList.remove('cart_active');
         main.classList.remove('main_hidden');
+        goodsDetails.style.display = 'block';
     })
 }
 
@@ -51,13 +55,14 @@ function addGoodsToCart () { //добавление и удаление това
             let currentAddGood = {} as ICartGood;
             currentAddGood.id = id;
             currentAddGood.count = 1;
-            currentAddGood.price = goodsList[id -1].price;
+            currentAddGood.price = goodsList2[id -1].price;
             currentAddGood.totalPrice = currentAddGood.count * currentAddGood.price;
-            currentAddGood.stock = goodsList[id -1].stock;
+            currentAddGood.stock = goodsList2[id -1].stock;
             
             if (cart.length === 0) {
                 cart.push(currentAddGood);
-                console.log('cart', cart);
+
+                showGoodsInCart();
             } else {
                 let isInCart: boolean = false;
                 for (let i = 0; i < cart.length; i ++) {
@@ -67,30 +72,32 @@ function addGoodsToCart () { //добавление и удаление това
                 }
                 if (isInCart === false) {
                     cart.push(currentAddGood);
-                    console.log('cart', cart);
+
+                    showGoodsInCart();
                 } else {
                     const index: number = cart.indexOf(currentAddGood);
                     cart.splice(index, 1);
-                    console.log('cart', cart);
+
+                    showGoodsInCart();
                 }
             }
         })
     }
 
     buttonAddToCardDescr?.addEventListener('click', () => {
-        console.log('idCartDescr', idCartDescr)
         const id: number = idCartDescr;
 
         let currentAddGood = {} as ICartGood;
         currentAddGood.id = id;
         currentAddGood.count = 1;
-        currentAddGood.price = goodsList[id -1].price;
+        currentAddGood.price = goodsList2[id -1].price;
         currentAddGood.totalPrice = currentAddGood.count * currentAddGood.price;
-        currentAddGood.stock = goodsList[id -1].stock;
+        currentAddGood.stock = goodsList2[id -1].stock;
 
         if (cart.length === 0) {
             cart.push(currentAddGood);
-            console.log('cart', cart);
+
+            showGoodsInCart();
         } else {
             let isInCart: boolean = false;
             for (let i = 0; i < cart.length; i ++) {
@@ -100,11 +107,13 @@ function addGoodsToCart () { //добавление и удаление това
             }
             if (isInCart === false) {
                 cart.push(currentAddGood);
-                console.log('cart', cart);
+
+                showGoodsInCart();
             } else {
                 const index: number = cart.indexOf(currentAddGood);
                 cart.splice(index, 1);
-                console.log('cart', cart);
+
+                showGoodsInCart();
             }
         }
     })
@@ -130,7 +139,111 @@ function getIdGoodDescr () {
     }
 }
 
-// getIdGoodDescr();
+
+function showGoodsInCart() {
+    const cartItems = document.querySelector('.cart__items') as HTMLDivElement;
+
+    console.log(cart)
+
+    while (cartItems.firstChild) {
+        cartItems.removeChild(cartItems.firstChild);
+    }
+    
+    for (let i = 0; i < cart.length; i ++) {
+        console.log(goodsList2)
+        console.log(goodsList2[0].id)
+
+        const id: number = cart[i].id;
+        console.log(id)
+        const images = goodsList2[id - 1].images;
+        const title: string = goodsList2[id - 1].title;
+        
+        console.log(title)
+        const description: string = goodsList2[id - 1].description;
+        const rating: number = goodsList2[id - 1].rating;
+        const discount: number = goodsList2[id - 1].discountPercentage;
+        const stock: number = goodsList2[id - 1].stock;
+        const price: number = goodsList2[id - 1].price;
+        const totalPrice: number = cart[i].totalPrice;
+        const count: number = cart[i].count;
+
+        const divItemFromCart = document.createElement('div') as HTMLDivElement;
+        divItemFromCart.classList.add('cart__item');
+
+        const divNumber = document.createElement('div') as HTMLDivElement;
+        divNumber.classList.add('cart__item__number');
+        divNumber.textContent = String(i + 1);
+
+        const divImg = document.createElement('div') as HTMLDivElement;
+        divImg.classList.add('cart__item__img');
+        divImg.style.backgroundImage = `url(${images[0]})`;
+
+        const divWrapper = document.createElement('div') as HTMLDivElement;
+        divWrapper.classList.add('cart__item__wrapper');
+
+        const divTitle = document.createElement('div') as HTMLDivElement;
+        divTitle.classList.add('cart__item__title');
+        divTitle.textContent = `${title}`;
+
+        const divDescription = document.createElement('div') as HTMLDivElement;
+        divDescription.classList.add('cart__item__description');
+        divDescription.textContent = `${description}`;
+
+        const divContainer = document.createElement('div') as HTMLDivElement;
+        divContainer.classList.add('cart__item__container');
+
+        const divRating = document.createElement('div') as HTMLDivElement;
+        divRating.classList.add('cart__item__rating');
+        divRating.textContent = `Rating: ${rating}`;
+
+        const divDiscount= document.createElement('div') as HTMLDivElement;
+        divDiscount.classList.add('cart__item__discount');
+        divDiscount.textContent = `Discount: ${discount}%`;
+
+        const divWrapper2 = document.createElement('div') as HTMLDivElement;
+        divWrapper2.classList.add('cart__item__wrapper__second');
+
+        const divStock = document.createElement('div') as HTMLDivElement;
+        divStock.classList.add('cart__item__stock');
+        divStock.textContent = `Stock: ${stock}%`;
+
+        const divContainer2 = document.createElement('div') as HTMLDivElement;
+        divContainer2.classList.add('cart__item__container__second');
+
+        const divSmaller = document.createElement('div') as HTMLDivElement;
+        divSmaller.classList.add('cart__item__smaller');
+        divSmaller.textContent = '-';
+
+        const divCount = document.createElement('div') as HTMLDivElement;
+        divCount.classList.add('cart__item__count');
+        divCount.textContent = String(count);
+
+        const divMore = document.createElement('div') as HTMLDivElement;
+        divMore.classList.add('cart__item__more');
+        divMore.textContent = '+';
+
+        const divTotalPrice = document.createElement('div') as HTMLDivElement;
+        divTotalPrice.classList.add('cart__item__total-price');
+        divTotalPrice.textContent = `€ ${totalPrice}`;
+
+        cartItems.appendChild(divItemFromCart);
+        divItemFromCart.appendChild(divNumber);
+        divItemFromCart.appendChild(divImg);
+        divItemFromCart.appendChild(divWrapper);
+        divWrapper.appendChild(divTitle);
+        divWrapper.appendChild(divDescription);
+        divWrapper.appendChild(divContainer);
+        divContainer.appendChild(divRating);
+        divContainer.appendChild(divDiscount);
+        divItemFromCart.appendChild(divWrapper2);
+        divWrapper2.appendChild(divStock);
+        divWrapper2.appendChild(divContainer2);
+        divContainer2.appendChild(divSmaller);
+        divContainer2.appendChild(divCount);
+        divContainer2.appendChild(divMore);
+        divWrapper2.appendChild(divTotalPrice);
+    }
+}
 
 
 cartOpen ();
